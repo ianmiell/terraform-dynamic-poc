27808c7149b558be373d47e311cd74fc726572dc
Ian Miell <ian.miell@gmail.com>
2017-02-21 20:29:48 +0000

    destroy

--- create_dynamic_environment.sh
+++ create_dynamic_environment.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# Ensure we are in the right folder
 pushd $(dirname ${BASH_SOURCE[0]})
 
+# Create a (probably) unique ID by concatenating two random values (RANDOM is a variable inherent to bash), with the day of year as a suffix.
 ID="dynamic_environment_${RANDOM}${RANDOM}_$(date +%j)"
+
+# Create the terraform folder.
 mkdir -p ${ID}
 pushd ${ID}
 cat > main.tf << END
@@ -10,11 +15,14 @@ module "dynamicenv" {
   dynamic_env_id     = "${ID}"
 }
 END
+
+# Terraform ahoy!
 terraform get
 terraform plan
 terraform apply
 popd
 
+# Record the creation in git and push. Assumes keys set up.
 git add ${ID}
 git commit -am "${ID} environment added"
 git push
--- destroy_dynamic_environment.sh
+++ destroy_dynamic_environment.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
-set -x
-set -e
+
+# We need extended glob capabilities.
 shopt -s extglob
+
+# Ensure we are in the right folder
+pushd $(dirname ${BASH_SOURCE[0]})
+
+# Default to destroying environments over 7 days old.
+# If you want to destroy all of them, pass in '-1' as an argument.
 DAYS=${1:-7}
 TODAY=$(date +%j)
+# Remove leading zeroes from the date.
 TODAY=${TODAY##+(0)}
+
+# Go through all the environment folders, and terraform destroy, git remove and remove the folder.
 for dir in $(find dynamic_environment_* -type d -maxdepth 0)
 do
- dir_day=${dir##*_}
- dir_day=${dir_day##+(0)}
-    if [[ $(( $TODAY - ${dir_day})) -gt $DAYS ]]
- then
-  pushd "${dir}"
-  terraform destroy -force
-  popd
-  git rm -rf "${dir}"
-  git commit -am "destroyed ${dir}"
-  git push
-  rm -rf "${dir}"
- fi
+	# Remove the folder prefix.
+	dir_day=${dir##*_}
+	# Remove any leading zeroes from the day of year.
+	dir_day=${dir_day##+(0)}
+	# If over 7 days old.
+	if [[ $(( ${TODAY} - ${dir_day})) -gt ${DAYS} ]]
+	then
+		pushd "${dir}"
+		# Destroy the environment.
+		terraform destroy -force
+		popd
+		# Remove from git.
+		git rm -rf "${dir}"
+		git commit -am "destroyed ${dir}"
+		git push
+		# Remove left-over backup files.
+		rm -rf "${dir}"
+	fi
 done
-
